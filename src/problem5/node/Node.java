82355879b46989c7d7f584009f3dc6e165832956
maheshwarinaman <49190119+maheshwarinaman@users.noreply.github.com>
2020-03-28 16:09:30 +0530

    Update Node.java

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -7,4 +7,30 @@
 package problem5.node;
 // to define node properties
 public class Node {
+  
+
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
