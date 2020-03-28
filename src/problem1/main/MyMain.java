79b9e6100c6645781d350d56736e8ea870ae1ba8
maheshwarinaman <49190119+maheshwarinaman@users.noreply.github.com>
2020-03-28 14:28:22 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,12 +7,14 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
    public static void main(String[] args) {
         MyBinarySearchTree binarySearchTree = new MyBinarySearchTree();
         binarySearchTree.insert(15);
-        binarySearchTree.insert(10);
-        binarySearchTree.insert(50);
+        binarySearchTree.insert(17);
+        binarySearchTree.insert(35);
         binarySearchTree.insert(55);
         binarySearchTree.insert(60);
 
